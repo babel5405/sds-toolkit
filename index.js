aacfe3efdc0f1419cb9459fe5ef875449ac1ce3a
Bricyn <bricyna@naetyr.com>
2019-07-14 23:33:03 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,168 @@
+const { exec } = require('child_process');
+let Config = require("./config.json");
+let Chokidar = null;
+let BrowserSync = null;
+let status = new Object;
+// Want to make a default library of configs.
+// if (!require("./config.json")) {
+//     Config = require("./lib/Config.js");
+// } else {
+//     Config = require("./config.json");
+// }
+
+if (Config.Web.Enable) {
+    BrowserSync = require("browser-sync").create();
+}
+
+if (Config.File.Enable) {
+    Chokidar = require("Chokidar");
+}
+
+if (Chokidar != null) {
+    let Watcher;
+    let ready = false;
+
+    if (Config.File.Path == "./") {
+        Config.File.Path = process.cwd();
+    }
+
+    if (Config.File.ignoreDots == true) {
+        Watcher = Chokidar.watch(Config.File.Path, {ignored: Config.File.Excluded, persistent: true});
+    } else {
+        Watcher = Chokidar.watch(Config.File.Path, {persistent: true});
+    }
+
+    Watcher.on('ready', function() {
+        console.log("File Watcher Loaded");
+        ready = true;
+    })
+
+    Watcher.on('add', function(path) {
+        if (ready) {
+            console.log('File', path, 'has been added');
+            RunCommands(Config.File.Commands.Add).then(function(response) {
+                ReloadWeb()
+            }).catch(function(response) {
+                logErr(response);
+            });
+        }
+    });
+
+    Watcher.on('addDir', function(path) {
+        if (ready) {
+            console.log('Directory', path, 'has been added');
+            RunCommands(Config.File.Commands.Add).then(function(response) {
+                ReloadWeb()
+            }).catch(function(response) {
+                logErr(response);
+            });
+        }
+    });
+
+    Watcher.on('change', function(path) {
+        if (ready) {
+            console.log('File', path, 'has been changed');
+            RunCommands(Config.File.Commands.Save).then(function(response) {
+                ReloadWeb()
+            }).catch(function(response) {
+                logErr(response);
+            });
+        }
+        
+    });
+
+    Watcher.on('unlink', function(path) {
+        if (ready) {
+            console.log('File', path, 'has been removed');
+            RunCommands(Config.File.Commands.Remove).then(function(response) {
+                ReloadWeb()
+            }).catch(function(response) {
+                logErr(response);
+            });
+        }
+        
+    });
+
+    Watcher.on('unlinkDir', function(path) {
+        if (ready) {
+            console.log('Directory', path, 'has been removed');
+            RunCommands(Config.File.Commands.Remove).then(function(response) {
+                ReloadWeb()
+            }).catch(function(response) {
+                logErr(response);
+            });
+        }
+        
+    });
+
+    Watcher.on('error', function(error) {
+        console.error('Error happened', error);
+    });
+}
+
+if (BrowserSync) {
+
+    if (Config.Web.Path == "./") {
+        Config.Web.Path = process.cwd();
+    }
+
+    BrowserSync.init({
+        server: Config.Web.Path,
+        port: 5500
+    });
+}
+
+function ReloadWeb() {
+    setTimeout(BrowserSync.reload(), Config.Web.ReloadDelay * 1000 + 1); // We use +1 to ensure the timeout is never 0
+}
+
+function RunCommands(commands) {
+    Configs = Config;
+    return new Promise(function(resolve, reject) {
+        commands.forEach(function(command) {
+            try {
+                ExecuteCommand(command[0], command[1]).then(function(result) {
+                    if (result == true) {
+                        resolve(true);
+                    } else {
+                        reject(command);
+                    }
+                });
+            } catch (e) {
+                console.log(e.message);
+                reject(command);
+            }
+        });
+    });
+}
+
+function ExecuteCommand(command, delay) {
+        return new Promise(function(resolve, reject) {
+            if (!command) {
+                throw new Error("Invalid Command Config (Command Missing)");
+            }
+
+            if (delay != 0 && !delay) {
+                throw new Error("Invalid command Config (Delay Missing): " + command);
+            }
+
+            setTimeout(function() {
+                console.log("Running Command: " + command);
+                exec(command, (error, stdout, stderr) => {
+                    if (error) {
+                        reject(`exec error: ${error}`)
+                    }
+                    console.log(`stdout: ${stdout}`);
+                    console.log(`stderr: ${stderr}`);
+                    resolve(true)
+                });
+            }, delay * 1000);
+        });
+}
+
+// Supress non-breaking error message from fs.
+function logErr(response) {
+    if (response != "TypeError [ERR_INVALID_CALLBACK]: Callback must be a function") {
+        console.log("An Error Occurred: " + response);
+    }
+}
